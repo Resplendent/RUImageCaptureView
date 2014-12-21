bdde0caafc6dffff815122e92a3cc47fbec6eab9
BenMaer <ben@resplendent.co>
2014-12-21 13:45:15 -0500

    Added project and files.

---
+++ RUImageCaptureView/RUImageCaptureView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RUImageCaptureView
+//
+//  Created by Benjamin Maer on 12/21/14.
+//  Copyright (c) 2014 Resplendent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
