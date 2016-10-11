0afd86912e53ae46e8ded4ee890e22b86fbd302a
Benjamin Maer <ben@resplendent.co>
2016-10-11 12:53:39 -0700

    Added files

--- Example/Pods/Target Support Files/RUImageCaptureView/RUImageCaptureView-umbrella.h
+++ Example/Pods/Target Support Files/RUImageCaptureView/RUImageCaptureView-umbrella.h
@@ -2,6 +2,10 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "AVCaptureDevice+RUCaptureDevices.h"
+#import "AVCaptureOutput+RUGetAVCaptureConnection.h"
+#import "RUImageCaptureView.h"
+#import "RUImageCaptureViewProtocols.h"
 
 FOUNDATION_EXPORT double RUImageCaptureViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char RUImageCaptureViewVersionString[];
